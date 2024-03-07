c882a9df2ae3a3bdfdf339d1d4c4d99918203102
emils <emils@Bonitos_bumstik>
2024-03-07 22:28:43 +0200

    I added mervienibas thingy to code

--- mervienibas.html.js
+++ mervienibas.html.js
@@ -1,6 +1,11 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
+function ToMebibytes() {
+  var gigabytes = parseFloat(document.getElementById("inputField").value);
+  var mebibytes = gigabytes * 1024; 
+  document.getElementById("outputField1").textContent = mebibytes.toFixed(2) + " MiB";
+}
 
-
+function toMegabytes() {
+  var gigabytes = parseFloat(document.getElementById("inputField").value);
+  var megabytes = gigabytes * 1000; 
+  document.getElementById("outputField2").textContent = megabytes.toFixed(2) + " MB";
+}
